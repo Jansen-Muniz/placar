90bec664d23b1e92fc75e32164ce37379c229de3
Jansen Muniz <92958989+Jansen-Muniz@users.noreply.github.com>
2023-09-27 12:20:08 -0300

    Alteração no app.js

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const cronometro = document.querySelector('#cronometro')
 let contadorSegundos = 0
 let contadorMinutos = 0
+let timer = null
 
-setInterval(() => {
-  let segundos = contadorSegundos < 10 ? `0${contadorSegundos}` : contadorSegundos
-  let minutos = contadorMinutos < 10 ? `0${contadorMinutos}` : contadorMinutos
-
+timer = setInterval(() => {
   if (contadorSegundos > 59) {
     contadorSegundos = 0
     contadorMinutos++
   }
 
-  contadorSegundos++
+  contadorMinutos === 2
+    ? clearInterval(timer)
+    : false
+
+  let segundos = contadorSegundos < 10
+    ? `0${contadorSegundos}`
+    : contadorSegundos
+  let minutos = contadorMinutos < 10
+    ? `0${contadorMinutos}`
+    : contadorMinutos
 
   cronometro.innerHTML = `<span id="minutos">${minutos}</span>:<span id="segundos">${segundos}</span>`
-}, 500);
+
+  contadorSegundos++
+}, 1000);
